ccae278dc13ebe3258a6da25c5da6f31423decd2
Michael Pritchard <michael.pritchard@csiro.au>
2022-06-06 15:53:41 +1000

    add serial class and implement main methods, need to test

--- greenseeker_driver/include/greenseeker_driver/greenseeker.hpp
+++ greenseeker_driver/include/greenseeker_driver/greenseeker.hpp
@@ -3,19 +3,32 @@
 #include <ros/ros.h>
 #include <greenseeker_msgs/GreenSeekerPacket.h>
 
+#include "simple_serial.h"
+
 namespace greenseeker_driver
 {
 class GreenSeeker
 {
 public:
-  GreenSeeker(ros::NodeHandle nh, ros::NodeHandle pnh) : nh_(nh), pnh_(pnh), loop_rate_(200.0)
+  GreenSeeker(ros::NodeHandle nh, ros::NodeHandle pnh) : nh_(nh), pnh_(pnh), loop_rate_(100.0), frame_id_("greenseeker")
   {
     // get loop rate
+    loop_rate_ = pnh_.param("loop_rate", loop_rate_);
+    // frame id
+    frame_id_ = pnh_.param("frame_id", frame_id_);
 
     // get serial connection details
+    if (!pnh_.getParam("port", port_) && !pnh_.getParam("baud", baud_))
+    {
+      ROS_ERROR("greenseeker_driver missing serial parametes");
+      return;
+    }
 
     // try and open serial port
     connect();
+
+    // create a greenseeker packet message publisher
+    packet_pub_ = pnh.advertise<greenseeker_msgs::GreenSeekerPacket>("packet", 20);
 
     // start polling timer
     read_poll_timer_ = pnh_.createTimer(loop_rate_.expectedCycleTime(), &GreenSeeker::read_timer_cb, this);
@@ -37,12 +50,14 @@ private:
   ros::NodeHandle nh_;
   ros::NodeHandle pnh_;
   ros::Rate loop_rate_;
+  std::string frame_id_;
 
   // connection details
   std::string port_;
   int baud_;
 
   // serial port
+  std::shared_ptr<SimpleSerial> serial_;
 
   // packet publisher
   ros::Publisher packet_pub_;
--- greenseeker_driver/src/greenseeker.cpp
+++ greenseeker_driver/src/greenseeker.cpp
@@ -5,14 +5,72 @@ namespace greenseeker_driver
 
 const bool GreenSeeker::connect()
 {
+  try
+  {
+    serial_ = std::make_shared<SimpleSerial>(port_, baud_);
+    return true;
+  }
+  catch (boost::system::system_error& e)
+  {
+    ROS_ERROR_STREAM("Error: " << e.what());
+    return false;
+  }
 }
 
 const bool GreenSeeker::disconnect()
 {
+  serial_.reset();
 }
 
 const bool GreenSeeker::read()
 {
+  try
+  {
+    // read serial
+    std::string line = serial_->readLine();
+
+    // create a packet message
+    greenseeker_msgs::GreenSeekerPacket msg;
+    msg.header.stamp = ros::Time::now();
+    msg.header.frame_id = frame_id_;
+
+    // decode packet
+    // split by comma
+    std::string delimiter = ",";
+
+    // time
+    msg.time = std::strtoull(line.substr(0, line.find(delimiter)).c_str(), NULL, 0);
+    line.erase(0, line.find(delimiter) + delimiter.length());
+    // plot
+    msg.plot = std::strtoll(line.substr(0, line.find(delimiter)).c_str(), NULL, 0);
+    line.erase(0, line.find(delimiter) + delimiter.length());
+    // count
+    msg.count = std::strtoll(line.substr(0, line.find(delimiter)).c_str(), NULL, 0);
+    line.erase(0, line.find(delimiter) + delimiter.length());
+    // ndvi
+    msg.ndvi = std::stod(line.substr(0, line.find(delimiter)));
+    line.erase(0, line.find(delimiter) + delimiter.length());
+    // irvi
+    msg.irvi = std::stod(line.substr(0, line.find(delimiter)));
+    line.erase(0, line.find(delimiter) + delimiter.length());
+
+    // red_rfl
+    msg.red_rfl = std::stod(line.substr(0, line.find(delimiter)));
+    line.erase(0, line.find(delimiter) + delimiter.length());
+    // nir_rfl
+    msg.nir_rfl = std::stod(line.substr(0, line.find(delimiter)));
+    line.erase(0, line.find(delimiter) + delimiter.length());
+
+    // publish message
+    packet_pub_.publish(msg);
+
+    return true;
+  }
+  catch (boost::system::system_error& e)
+  {
+    ROS_ERROR_STREAM("Error: " << e.what());
+    return false;
+  }
 }
 
 void GreenSeeker::read_timer_cb(const ros::TimerEvent& event)
